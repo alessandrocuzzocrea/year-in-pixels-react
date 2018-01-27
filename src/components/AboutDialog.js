fb67ca8e652fa42aeacf1700ad74cc5d69e652b4
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-01-28 00:53:38 +0100

    Fix lint warning (Using target="_blank" without rel="noopener noreferrer" is a security risk: see https://mathiasbynens.github.io/rel-noopener)

--- src/components/AboutDialog.js
+++ src/components/AboutDialog.js
@@ -6,11 +6,11 @@ const AboutDialog = (props) => {
         <div id="aboutDialog" className="dialog" style={props.style}>
             <a href="#" className="close" onClick={() => props.closeDialog()}>X</a>
             <h3>year-in-pixels-react</h3>
-            <p>Remixed by <a href="https://twitter.com/alcuzzocrea" target="_blank">@alcuzzocrea</a></p>
-            <p>This is a complete React porting of <a href="https://year-in-pixels.glitch.me/" target="_blank">Year in Pixel</a></p>
-            <p><a href="https://github.com/alessandrocuzzocrea/year-in-pixels-react" target="_blank">Github</a></p>
+            <p>Remixed by <a href="https://twitter.com/alcuzzocrea" target="_blank" rel="noopener noreferrer">@alcuzzocrea</a></p>
+            <p>This is a complete React porting of <a href="https://year-in-pixels.glitch.me/" target="_blank" rel="noopener noreferrer">Year in Pixel</a></p>
+            <p><a href="https://github.com/alessandrocuzzocrea/year-in-pixels-react" target="_blank" rel="noopener noreferrer">Github</a></p>
             <h4>Original App by:</h4>
-            <p>Alejandro AR (<a href="http://kinduff.com" target="_blank">@kinduff</a>)</p>
+            <p>Alejandro AR (<a href="http://kinduff.com" target="_blank" rel="noopener noreferrer">@kinduff</a>)</p>
         </div>
     );
 }
