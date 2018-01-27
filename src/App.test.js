a65ecbac561f88bdcd1b87c6e263d4fcd5c6b764
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-01-27 16:02:08 +0100

    Add fetch-mock and Quotes REST API mock data

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import App from './App';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+var fetchMock = require('fetch-mock');
+var qodMockData = require('../fixtures/qodMockData')
+fetchMock.get('https://quotes.rest/qod', qodMockData);
+
+global.window = {}
+import localStorage from 'mock-local-storage'
+import { request } from 'https';
+window.localStorage = global.localStorage
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />);
+    });
+});
