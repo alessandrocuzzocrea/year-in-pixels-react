fef4b5edaad8d11b5324b5040cfc1fc0e93a90a8
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-01-28 00:49:51 +0100

    Fix lint warning (Expected '===' and instead saw '==')

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 export function daysInYear(year) {
-    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+    if (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)) {
         return 366;
     } else {
         return 365
