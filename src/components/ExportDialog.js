fe640d2a0995105250f2ce2f9d54a9a725a02ada
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-01-28 12:28:59 +0100

    Refactor ExportDialog

--- src/components/ExportDialog.js
+++ src/components/ExportDialog.js
@@ -1,26 +1,31 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class ExportDialog extends React.Component {
-    render() {
 
-        const { days } = this.props;
-
-        const res = Object
-            .keys(this.props.days)
+    daysToString = (days) => {
+        return Object
+            .keys(days)
             .map((val) => { return days[val] })
             .join("");
+    };
 
+    render() {
         return (
             <div id="exportDialog" className="dialog" style={this.props.style}>
                 <a href="#" className="close" onClick={() => this.props.closeDialog()}>X</a>
                 <h3>Export a mood calendar</h3>
                 <p>Copy the following text and use the import tool to import it.</p>
                 <textarea ref={(el) => this.textArea = el} id="exportMoodText" onClick={() => { this.textArea.select() }}
-                    readOnly="readonly" value={res}></textarea>
+                    readOnly="readonly" value={this.daysToString(this.props.days)}></textarea>
             </div>
         );
-    }
+    };
+};
+
+ExportDialog.propTypes = {
+    days: PropTypes.object.isRequired,
+    closeDialog: PropTypes.func.isRequired,
 }
 
 export default ExportDialog;
--- src/components/ExportDialog.test.js
+++ src/components/ExportDialog.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-import moodCalendarMockData from '../../fixtures/moodCalendarMockData';
+import { days, daysString, noOfDays } from '../../fixtures/moodCalendarMockData';
 
 import ExportDialog from './ExportDialog';
 
@@ -16,9 +16,41 @@ describe('<ExportDialog />', () => {
 
         const wrapper = shallow(
             <ExportDialog
-                days={moodCalendarMockData}
+                days={days}
                 closeDialog={mockFn}
             />
         );
+    });
+
+    it('convert days object to string', () => {
+
+        const wrapper = shallow(
+            <ExportDialog
+                days={days}
+                closeDialog={jest.fn()}
+            />
+        );
+
+        expect(wrapper.instance().daysToString(days)).toEqual(daysString);
+    });
+
+    it('select textarea on click', () => {
+
+        const wrapper = mount(
+            <ExportDialog
+                days={days}
+                closeDialog={jest.fn()}
+            />
+        );
+
+        const textArea = wrapper.find('textarea');
+
+        expect(textArea.instance().selectionStart).toEqual(0);
+        expect(textArea.instance().selectionEnd).toEqual(0);
+
+        textArea.simulate('click');
+
+        expect(textArea.instance().selectionStart).toEqual(0);
+        expect(textArea.instance().selectionEnd).toEqual(noOfDays);
     });
 });
