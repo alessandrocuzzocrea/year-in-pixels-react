7ec8f626172a1ef2ed3036ca4f29296b02f448c9
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-01-30 10:50:11 +0100

    Refactor Chart Component to SFC

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,71 +5,68 @@ import ChartistGraph from 'react-chartist';
 import { dayToMonth } from '../helpers';
 import { moodOptions } from '../consts';
 
-class Chart extends React.Component {
+export const getMonthMoodAvgArr = (days) => {
 
-  getMonthMoodAvgArr = (days) => {
+  const initialValue = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [] };
 
-    const initialValue = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [] };
+  const res0 = Object.keys(days).reduce((acc, val) => {
+    const currMonth = dayToMonth(parseInt(val, 10));
+    const moodValue = days[val];
+    if (moodValue !== 0) acc[currMonth].push(days[val]);
+    return acc;
+  }, initialValue);
+  const res1 = Object.keys(res0).map((val) => {
+    return res0[val];
+  });
+  const res2 = res1.map(array => {
+    return array.reduce(function (acc, val) { return acc + val; }, 0) / array.length || 0;
+  });
 
-    const res0 = Object.keys(days).reduce((acc, val) => {
-      const currMonth = dayToMonth(parseInt(val, 10));
-      const moodValue = days[val];
-      if (moodValue !== 0) acc[currMonth].push(days[val]);
-      return acc;
-    }, initialValue);
-    const res1 = Object.keys(res0).map((val) => {
-      return res0[val];
-    });
-    const res2 = res1.map(array => {
-      return array.reduce(function (acc, val) { return acc + val; }, 0) / array.length || 0;
-    });
+  return res2;
+};
 
-    return res2;
+export const labelInterpolation = (_, index) => {
+  return moodOptions[index];
+};
+
+const Chart = (props) => {
+
+  const { days } = props;
+
+  const data = {
+    labels: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+    series: [getMonthMoodAvgArr(days)],
   };
 
-  labelInterpolation = (_, index) => {
-    return moodOptions[index];
+  const options = {
+    axisY: {
+      onlyInteger: true,
+      labelInterpolationFnc: labelInterpolation,
+      stretch: true,
+      offset: 50
+    },
+    axisX: {
+      offset: 20
+    },
+    high: 6,
+    low: 0,
+    showArea: true,
+    showLine: true,
+    showPoint: false,
+    fullWidth: true,
+    chartPadding: {
+      top: 0,
+      right: 10
+    }
   };
 
-  render() {
+  const type = 'Line';
 
-    const { days } = this.props;
-
-    const data = {
-      labels: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
-      series: [this.getMonthMoodAvgArr(days)],
-    };
-
-    const options = {
-      axisY: {
-        onlyInteger: true,
-        labelInterpolationFnc: this.labelInterpolation,
-        stretch: true,
-        offset: 50
-      },
-      axisX: {
-        offset: 20
-      },
-      high: 6,
-      low: 0,
-      showArea: true,
-      showLine: true,
-      showPoint: false,
-      fullWidth: true,
-      chartPadding: {
-        top: 0,
-        right: 10
-      }
-    };
-
-    const type = 'Line';
-
-    return (
-      <div id="chart">
-        <ChartistGraph data={data} options={options} type={type} />
-      </div>
-    );
-  }
+  return (
+    <div id="chart">
+      <ChartistGraph data={data} options={options} type={type} />
+    </div>
+  );
 }
 
 Chart.propTypes = {
--- src/components/Chart.test.js
+++ src/components/Chart.test.js
@@ -7,28 +7,33 @@ configure({ adapter: new Adapter() });
 import { days, daysAvg } from '../../fixtures/moodCalendarMockData';
 import { moodOptions } from '../consts';
 
-import Chart from './Chart';
+import Chart, { getMonthMoodAvgArr, labelInterpolation } from './Chart';
 
 describe('<Chart />', () => {
 
     it('works correctly', () => {
         const wrapper = shallow(<Chart days={days} />);
     });
+});
+
+describe('getMonthModdAvg', () => {
 
     it('getMonthMoodAvgArr give the expected result', () => {
-        const wrapper = shallow(<Chart days={days} />);
-        const avr = wrapper.instance().getMonthMoodAvgArr(days);
+
+        const avr = getMonthMoodAvgArr(days);
         expect(avr).toEqual(daysAvg);
     });
+});
+
+describe('labelInterpolation', () => {
 
     it('labelInterpolation returns the correct values', () => {
 
-        const wrapper = shallow(<Chart days={days} />);
-        expect(wrapper.instance().labelInterpolation(null, 0)).toEqual(moodOptions[0]);
-        expect(wrapper.instance().labelInterpolation(null, 1)).toEqual(moodOptions[1]);
-        expect(wrapper.instance().labelInterpolation(null, 2)).toEqual(moodOptions[2]);
-        expect(wrapper.instance().labelInterpolation(null, 3)).toEqual(moodOptions[3]);
-        expect(wrapper.instance().labelInterpolation(null, 4)).toEqual(moodOptions[4]);
-        expect(wrapper.instance().labelInterpolation(null, 5)).toEqual(moodOptions[5]);
+        expect(labelInterpolation(null, 0)).toEqual(moodOptions[0]);
+        expect(labelInterpolation(null, 1)).toEqual(moodOptions[1]);
+        expect(labelInterpolation(null, 2)).toEqual(moodOptions[2]);
+        expect(labelInterpolation(null, 3)).toEqual(moodOptions[3]);
+        expect(labelInterpolation(null, 4)).toEqual(moodOptions[4]);
+        expect(labelInterpolation(null, 5)).toEqual(moodOptions[5]);
     });
 });
