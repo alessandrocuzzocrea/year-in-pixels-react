4fb8634004eaa919f147134bf5bc77004d3b7203
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-02 22:44:48 +0100

    Refactor App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -18,6 +18,10 @@ import { days, daysInitialState, daysString } from '../fixtures/moodCalendarMock
 
 describe('<App />', () => {
 
+    beforeEach(() => {
+        window.localStorage.clear();
+    });
+
     it('renders without crashing', () => {
         const wrapper = shallow(<App />);
     });
@@ -50,202 +54,268 @@ describe('<App />', () => {
         expect(days[activeMoodDay]).toEqual(5);
     });
 
-
-});
-
-
-it('has no dialog open on start', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('ImportDialog').length).toEqual(0);
-    expect(wrapper.find('ExportDialog').length).toEqual(0);
-    expect(wrapper.find('AboutDialog').length).toEqual(0);
-});
-
-describe('openDialog', () => {
-    it('sets state.openDialog value correctly', () => {
+    it('has no dialog open on start', () => {
         const wrapper = shallow(<App />);
+        expect(wrapper.find('ImportDialog').length).toEqual(0);
+        expect(wrapper.find('ExportDialog').length).toEqual(0);
+        expect(wrapper.find('AboutDialog').length).toEqual(0);
+    });
 
-        Object.keys(consts.dialogs).forEach((val) => {
-            wrapper.instance().openDialog(consts.dialogs.import);
-            expect(wrapper.state().openDialog).toEqual(consts.dialogs.import);
+    describe('openDialog', () => {
+        it('sets state.openDialog value correctly', () => {
+            const wrapper = shallow(<App />);
+
+            Object.keys(consts.dialogs).forEach((val) => {
+                wrapper.instance().openDialog(consts.dialogs.import);
+                expect(wrapper.state().openDialog).toEqual(consts.dialogs.import);
+            });
         });
     });
-});
 
-describe('closeDialog', () => {
-    it('sets state.openDialog to null', () => {
-        const wrapper = shallow(<App />);
+    describe('closeDialog', () => {
+        it('sets state.openDialog to null', () => {
+            const wrapper = shallow(<App />);
 
-        wrapper.instance().openDialog(consts.dialogs.import);
-        expect(wrapper.state().openDialog).toEqual(consts.dialogs.import);
+            wrapper.instance().openDialog(consts.dialogs.import);
+            expect(wrapper.state().openDialog).toEqual(consts.dialogs.import);
 
-        wrapper.instance().closeDialog();
-        expect(wrapper.state().openDialog).toEqual(null);
-    });
-});
-
-describe('askDemoDataConfirm', () => {
-
-    let confirmSpy;
-
-    beforeEach(() => {
-        confirmSpy = jest.spyOn(global, 'confirm');
+            wrapper.instance().closeDialog();
+            expect(wrapper.state().openDialog).toEqual(null);
+        });
     });
 
-    afterEach(() => {
-        confirmSpy.mockRestore();
+    describe('askDemoDataConfirm', () => {
+
+        let confirmSpy;
+
+        beforeEach(() => {
+            confirmSpy = jest.spyOn(global, 'confirm');
+        });
+
+        afterEach(() => {
+            confirmSpy.mockRestore();
+        });
+
+        it('asks for confirmation', () => {
+            const wrapper = shallow(<App />);
+
+            wrapper.instance().askDemoDataConfirm();
+            expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
+        });
+
+        it('calls fillDemoData if confirm', () => {
+            const wrapper = shallow(<App />);
+
+            const fillDemoDataSpy = jest.spyOn(wrapper.instance(), 'fillDemoData');
+            confirmSpy.mockReturnValue(true);
+
+            wrapper.instance().askDemoDataConfirm();
+            expect(fillDemoDataSpy).toHaveBeenCalled();
+
+            fillDemoDataSpy.mockReset();
+        });
+
+        it('does not call fillDemoData if not confirm', () => {
+            const wrapper = shallow(<App />);
+
+            const fillDemoDataSpy = jest.spyOn(wrapper.instance(), 'fillDemoData');
+            confirmSpy.mockReturnValue(false);
+
+            wrapper.instance().askDemoDataConfirm();
+            expect(fillDemoDataSpy).not.toHaveBeenCalled();
+
+            fillDemoDataSpy.mockReset();
+        });
     });
 
-    it('asks for confirmation', () => {
-        const wrapper = shallow(<App />);
+    describe('save and load data', () => {
 
-        wrapper.instance().askDemoDataConfirm();
-        expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
+        it('saves data to localStorage', () => {
+            const wrapper = shallow(<App />);
+            wrapper.setState({ days: days });
+            expect(JSON.parse(window.localStorage.getItem('moodCalendar'))).toEqual(days);
+        });
+
+        it('load data from localStorage', () => {
+            window.localStorage.setItem('moodCalendar', JSON.stringify(days));
+            const wrapper = shallow(<App />);
+            expect(wrapper.state().days).toEqual(days);
+        });
+
+        it('loadState must return null if JSON.parse throw an error', () => {
+            const wrapper = shallow(<App />);
+
+            window.localStorage.setItem('moodCalendar', null);
+            expect(wrapper.instance().loadState()).toEqual(null);
+
+            window.localStorage.setItem('moodCalendar', 'random_garbage');
+            expect(wrapper.instance().loadState()).toEqual(null);
+        });
+
     });
 
-    it('calls fillDemoData if confirm', () => {
-        const wrapper = shallow(<App />);
+    describe('askClearDataConfirm', () => {
 
-        const fillDemoDataSpy = jest.spyOn(wrapper.instance(), 'fillDemoData');
-        confirmSpy.mockReturnValue(true);
+        let confirmSpy;
 
-        wrapper.instance().askDemoDataConfirm();
-        expect(fillDemoDataSpy).toHaveBeenCalled();
+        beforeEach(() => {
+            confirmSpy = jest.spyOn(global, 'confirm');
+        });
 
-        fillDemoDataSpy.mockReset();
+        afterEach(() => {
+            confirmSpy.mockRestore();
+        });
+
+        it('asks for confirmation', () => {
+            const wrapper = shallow(<App />);
+
+            wrapper.instance().askClearDataConfirm();
+            expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
+        });
+
+        it('calls clearData if confirm', () => {
+            const wrapper = shallow(<App />);
+
+            const clearDataSpy = jest.spyOn(wrapper.instance(), 'clearData');
+            confirmSpy.mockReturnValue(true);
+
+            wrapper.instance().askClearDataConfirm();
+            expect(clearDataSpy).toHaveBeenCalled();
+
+            clearDataSpy.mockReset();
+        });
+
+        it('does not call clearData if not confirm', () => {
+            const wrapper = shallow(<App />);
+
+            const clearDataSpy = jest.spyOn(wrapper.instance(), 'clearData');
+            confirmSpy.mockReturnValue(false);
+
+            wrapper.instance().askClearDataConfirm();
+            expect(clearDataSpy).not.toHaveBeenCalled();
+
+            clearDataSpy.mockReset();
+        });
     });
 
-    it('does not call fillDemoData if not confirm', () => {
-        const wrapper = shallow(<App />);
+    describe('clear data', () => {
 
-        const fillDemoDataSpy = jest.spyOn(wrapper.instance(), 'fillDemoData');
-        confirmSpy.mockReturnValue(false);
+        it('clears data', () => {
+            const wrapper = shallow(<App />);
+            wrapper.setState({ days: days });
+            expect(wrapper.state().days).not.toEqual(daysInitialState);
+            wrapper.instance().clearData();
+            expect(wrapper.state().days).toEqual(daysInitialState);
+        });
 
-        wrapper.instance().askDemoDataConfirm();
-        expect(fillDemoDataSpy).not.toHaveBeenCalled();
+        it('asks for confirmation before clearing the data', () => {
+            const confirmSpy = jest.spyOn(global, 'confirm');
+            const wrapper = shallow(<App />);
 
-        fillDemoDataSpy.mockReset();
-    });
-});
+            wrapper.instance().askClearDataConfirm();
+            expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
 
-describe('save and load data', () => {
-
-    beforeEach(() => {
-        window.localStorage.clear();
-    });
-
-    it('saves data to localStorage', () => {
-        const wrapper = shallow(<App />);
-        wrapper.setState({ days: days });
-        expect(JSON.parse(window.localStorage.getItem('moodCalendar'))).toEqual(days);
+            confirmSpy.mockRestore();
+        });
     });
 
-    it('load data from localStorage', () => {
-        window.localStorage.setItem('moodCalendar', JSON.stringify(days));
-        const wrapper = shallow(<App />);
-        expect(wrapper.state().days).toEqual(days);
+    describe('isImportValid', () => {
+
+        it('validate correct import string', () => {
+            const wrapper = shallow(<App />);
+            expect(wrapper.instance().isImportValid(daysString)).toEqual(true);
+        });
+
+        it('does not validate null input', () => {
+            const wrapper = shallow(<App />);
+            expect(wrapper.instance().isImportValid(null)).toEqual(false);
+        });
+
+        it('does not validate input that does not match current year length', () => {
+            mockdate.set('2018/1/1');
+
+            const wrapper = shallow(<App />);
+            expect(wrapper.instance().isImportValid('0123456')).toEqual(false);
+        });
+
+        it('does not validate input that contains any characters except 0123456', () => {
+
+            const wrapper = shallow(<App />);
+            const invalidDaysString = daysString.replace('3', 'z');
+            expect(wrapper.instance().isImportValid(invalidDaysString)).toEqual(false);
+        });
     });
 
-    it('loadState must return null if JSON.parse throw an error', () => {
-        const wrapper = shallow(<App />);
+    describe('importData', () => {
 
-        window.localStorage.setItem('moodCalendar', null);
-        expect(wrapper.instance().loadState()).toEqual(null);
+        it('calls isImportValid', () => {
+            const wrapper = shallow(<App />);
 
-        window.localStorage.setItem('moodCalendar', 'random_garbage');
-        expect(wrapper.instance().loadState()).toEqual(null);
-    });
+            const mockIsImportValid = jest.spyOn(wrapper.instance(), 'isImportValid');
+            wrapper.instance().importData(daysString);
+            expect(mockIsImportValid).toHaveBeenCalled();
+            mockIsImportValid.mockRestore();
+        });
 
-});
+        it('asks for confirmation before setting the state', () => {
+            const confirmSpy = jest.spyOn(global, 'confirm');
+            const wrapper = shallow(<App />);
 
-describe('askClearDataConfirm', () => {
+            wrapper.instance().importData(daysString);
+            expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
 
-    let confirmSpy;
+            confirmSpy.mockRestore();
+        });
 
-    beforeEach(() => {
-        confirmSpy = jest.spyOn(global, 'confirm');
-    });
+        it('sets the state correctly', () => {
+            const wrapper = shallow(<App />);
+            const confirmSpy = jest.spyOn(global, 'confirm').mockReturnValue(true);
 
-    afterEach(() => {
-        confirmSpy.mockRestore();
-    });
+            expect(wrapper.state().days).not.toEqual(days);
 
-    it('asks for confirmation', () => {
-        const wrapper = shallow(<App />);
+            wrapper.instance().importData(daysString);
+            expect(wrapper.state().days).toEqual(days);
 
-        wrapper.instance().askClearDataConfirm();
-        expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
-    });
+            confirmSpy.mockRestore();
+        });
 
-    it('calls clearData if confirm', () => {
-        const wrapper = shallow(<App />);
+        it('calls closeDialog if the import was successful', () => {
+            const wrapper = shallow(<App />);
 
-        const clearDataSpy = jest.spyOn(wrapper.instance(), 'clearData');
-        confirmSpy.mockReturnValue(true);
+            const confirmSpy = jest.spyOn(global, 'confirm').mockReturnValue(true);
+            const closeDialogSpy = jest.spyOn(wrapper.instance(), 'closeDialog');
 
-        wrapper.instance().askClearDataConfirm();
-        expect(clearDataSpy).toHaveBeenCalled();
+            expect(wrapper.state().days).not.toEqual(days);
 
-        clearDataSpy.mockReset();
-    });
+            wrapper.instance().importData(daysString);
+            expect(closeDialogSpy).toHaveBeenCalled();
 
-    it('does not call clearData if not confirm', () => {
-        const wrapper = shallow(<App />);
+            confirmSpy.mockRestore();
+            closeDialogSpy.mockRestore();
+        });
 
-        const clearDataSpy = jest.spyOn(wrapper.instance(), 'clearData');
-        confirmSpy.mockReturnValue(false);
+        it('displays an alert if the import was unsuccessful', () => {
+            const wrapper = shallow(<App />);
 
-        wrapper.instance().askClearDataConfirm();
-        expect(clearDataSpy).not.toHaveBeenCalled();
+            const alertSpy = jest.spyOn(global, 'alert');
 
-        clearDataSpy.mockReset();
-    });
-});
+            wrapper.instance().importData(null);
+            expect(alertSpy).toHaveBeenCalledWith(consts.importErrorMsg);
 
-describe('clear data', () => {
+            alertSpy.mockRestore();
+        });
 
-    it('clears data', () => {
-        const wrapper = shallow(<App />);
-        wrapper.setState({ days: days });
-        expect(wrapper.state().days).not.toEqual(daysInitialState);
-        wrapper.instance().clearData();
-        expect(wrapper.state().days).toEqual(daysInitialState);
-    });
+        it('does not change the state import was unsuccessful', () => {
+            const wrapper = shallow(<App />);
+            wrapper.setState({ days });
+            expect(wrapper.state().days).not.toEqual(daysInitialState);
 
-    it('asks for confirmation before clearing the data', () => {
-        const confirmSpy = jest.spyOn(global, 'confirm');
-        const wrapper = shallow(<App />);
+            const alertSpy = jest.spyOn(global, 'alert').mockReturnValue(true);
+            wrapper.instance().importData(daysString);
 
-        wrapper.instance().askClearDataConfirm();
-        expect(confirmSpy).toHaveBeenCalledWith(consts.clearDataMsg);
+            expect(wrapper.state().days).toEqual(days);
 
-        confirmSpy.mockRestore();
-    });
-});
-
-describe('isImportValid', () => {
-
-    it('validate correct import string', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.instance().isImportValid(daysString)).toEqual(true);
-    });
-
-    it('does not validate null input', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.instance().isImportValid(null)).toEqual(false);
-    });
-
-    it('does not validate input that does not match current year length', () => {
-        mockdate.set('2018/1/1');
-
-        const wrapper = shallow(<App />);
-        expect(wrapper.instance().isImportValid('0123456')).toEqual(false);
-    });
-
-    it('does not validate input that contains any characters except 0123456', () => {
-
-        const wrapper = shallow(<App />);
-        const invalidDaysString = daysString.replace('3', 'z');
-        expect(wrapper.instance().isImportValid(invalidDaysString)).toEqual(false);
+            alertSpy.mockRestore();
+        });
     });
 });
