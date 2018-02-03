df0087b103d55732c3102d696e95ae30e0c8c609
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:21:15 +0100

    Run Prettier on MoodGrid.js

--- src/components/MoodGrid.js
+++ src/components/MoodGrid.js
@@ -1,46 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Month from './Month';
+import Month from "./Month";
 
 class MoodGrid extends React.Component {
+  getMonths = () => {
+    // prettier-ignore
+    const monthsInitials = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"];
+    return monthsInitials.map((initial, i) => {
+      return (
+        <Month
+          key={i}
+          monthNameInitial={initial}
+          month={i}
+          year={2018}
+          days={this.props.days}
+          activeDay={this.props.activeDay}
+          setActiveDay={this.props.setActiveDay}
+        />
+      );
+    });
+  };
 
-    getMonths = () => {
-        const monthsInitials = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"];
-        return monthsInitials.map( (initial, i) => {
-            return <Month key={i} monthNameInitial={initial} month={i} year={2018} days={this.props.days} activeDay={this.props.activeDay} setActiveDay={this.props.setActiveDay}/>
-        });
-    
-    }
-
-    getDays = () => {
-
-        return Array(31)
-            .fill()
-            .map((_, i) => {
-                return <span key={i} className="day">{i + 1}</span>
-            });
-    };
-
-    render() {
+  getDays = () => {
+    return Array(31)
+      .fill()
+      .map((_, i) => {
         return (
-            <div id="moodGrid" className="grid">
-                <div className="item">
-                    <span></span>
-                    <div className="days">
-                        {this.getDays()}
-                    </div>
-                </div>
-                {this.getMonths()}
-            </div>
+          <span key={i} className="day">
+            {i + 1}
+          </span>
         );
-    }
+      });
+  };
+
+  render() {
+    return (
+      <div id="moodGrid" className="grid">
+        <div className="item">
+          <span />
+          <div className="days">{this.getDays()}</div>
+        </div>
+        {this.getMonths()}
+      </div>
+    );
+  }
 }
 
 MoodGrid.propTypes = {
-    days: PropTypes.object.isRequired,
-    activeDay: PropTypes.number.isRequired,
-    setActiveDay: PropTypes.func.isRequired,
+  days: PropTypes.object.isRequired,
+  activeDay: PropTypes.number.isRequired,
+  setActiveDay: PropTypes.func.isRequired
 };
 
 export default MoodGrid;
