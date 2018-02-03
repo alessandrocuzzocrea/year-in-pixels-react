ec1efcb8352da11bbf83e02e711f01ac5c627360
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:21:54 +0100

    Run Prettier on QuoteOfTheDay.js

--- src/components/QuoteOfTheDay.js
+++ src/components/QuoteOfTheDay.js
@@ -1,44 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { QuotesRESTAPIUrl } from '../consts';
+import { QuotesRESTAPIUrl } from "../consts";
 
 class QuoteOfTheDay extends React.Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
-
-        this.state = {
-            quote: { quote: "Loading...", author: null },
-        };
-    }
-
-    componentWillMount() {
-
-        this.getQuote().then(quote => this.setState({ quote }));
-    }
-
-    getQuote = async () => {
-
-        try {
-            const res = await fetch(QuotesRESTAPIUrl);
-            const data = await res.json();
-            const { quote, author } = data['contents']['quotes'][0];
-            return { quote, author };
-        } catch (e) {
-            return { quote: `GET ${QuotesRESTAPIUrl} 429 (Too Many Requests)`, author: "quotes.rest" };
-        }
+    this.state = {
+      quote: { quote: "Loading...", author: null }
     };
+  }
 
-    render() {
+  componentWillMount() {
+    this.getQuote().then(quote => this.setState({ quote }));
+  }
 
-        const { quote, author } = this.state.quote;
-
-        return (
-            <div id="quoteOfTheDay">
-                <p>{quote}</p>{(author !== null) ? <p className="author">—{author}</p> : null}
-            </div>
-        );
+  getQuote = async () => {
+    try {
+      const res = await fetch(QuotesRESTAPIUrl);
+      const data = await res.json();
+      const { quote, author } = data["contents"]["quotes"][0];
+      return { quote, author };
+    } catch (e) {
+      return {
+        quote: `GET ${QuotesRESTAPIUrl} 429 (Too Many Requests)`,
+        author: "quotes.rest"
+      };
     }
+  };
+
+  render() {
+    const { quote, author } = this.state.quote;
+
+    return (
+      <div id="quoteOfTheDay">
+        <p>{quote}</p>
+        {author !== null ? <p className="author">—{author}</p> : null}
+      </div>
+    );
+  }
 }
 
 export default QuoteOfTheDay;
