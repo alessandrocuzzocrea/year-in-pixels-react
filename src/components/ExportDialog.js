d41a2182f9d57a40bdfd36dec5783b401ea086f0
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:20:06 +0100

    Run Prettier on ExportDialog.js

--- src/components/ExportDialog.js
+++ src/components/ExportDialog.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-export const daysToString = (days) => {
-    return Object
-        .keys(days)
-        .map((val) => { return days[val] })
-        .join("");
+export const daysToString = days => {
+  return Object.keys(days)
+    .map(val => {
+      return days[val];
+    })
+    .join("");
 };
 
-const ExportDialog = (props) => {
-
-    return (
-        <div id="exportDialog" className="dialog" style={props.style}>
-            <a href="#" className="close" onClick={() => props.closeDialog()}>X</a>
-            <h3>Export a mood calendar</h3>
-            <p>Copy the following text and use the import tool to import it.</p>
-            <textarea id="exportMoodText" onClick={e => e.target.select()}
-                readOnly="readonly" value={daysToString(props.days)}></textarea>
-        </div>
-    );
+const ExportDialog = props => {
+  return (
+    <div id="exportDialog" className="dialog" style={props.style}>
+      <a href="#" className="close" onClick={() => props.closeDialog()}>
+        X
+      </a>
+      <h3>Export a mood calendar</h3>
+      <p>Copy the following text and use the import tool to import it.</p>
+      <textarea
+        id="exportMoodText"
+        onClick={e => e.target.select()}
+        readOnly="readonly"
+        value={daysToString(props.days)}
+      />
+    </div>
+  );
 };
 
 ExportDialog.propTypes = {
-    days: PropTypes.object.isRequired,
-    closeDialog: PropTypes.func.isRequired,
-}
+  days: PropTypes.object.isRequired,
+  closeDialog: PropTypes.func.isRequired
+};
 
 export default ExportDialog;
