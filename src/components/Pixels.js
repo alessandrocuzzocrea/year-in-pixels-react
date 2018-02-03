d30f1c9bed884ece57959c47141f00ab1b57d82a
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:21:39 +0100

    Run Prettier on Pixels.js

--- src/components/Pixels.js
+++ src/components/Pixels.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React from "react";
 
 class Pixels extends React.Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super()
+    this.state = {
+      children: []
+    };
+  }
 
-        this.state = {
-            children: [],
-        };
+  componentDidMount() {
+    const pixelsWidth = this.pixels.clientWidth;
+    const pixelsHeight = this.pixels.clientHeight;
+
+    const children = [];
+    for (let i = 0; i < 25; i++) {
+      const pixelClass = "mood-" + Math.floor(Math.random() * (5 - 1 + 1) + 1);
+      const divStyle = {
+        top: Math.floor(Math.random() * (pixelsHeight - 20)),
+        left: Math.floor(Math.random() * (pixelsWidth - 20))
+      };
+      const child = <div key={i} className={pixelClass} style={divStyle} />;
+      children.push(child);
     }
 
-    componentDidMount() {
-        const pixelsWidth = this.pixels.clientWidth;
-        const pixelsHeight = this.pixels.clientHeight;
+    this.setState({ children });
+  }
 
-        const children = [];
-        for (let i = 0; i < 25; i++) {
-            const pixelClass = "mood-" + Math.floor(Math.random() * (5 - 1 + 1) + 1);;
-            const divStyle = {
-                top: Math.floor(Math.random() * (pixelsHeight - 20)),
-                left: Math.floor(Math.random() * (pixelsWidth - 20)),
-            }
-            const child = <div key={i} className={pixelClass} style={divStyle} />
-            children.push(child);
-        }
-
-        this.setState({ children });
-    }
-
-    render() {
-        return <div id="pixels" ref={(el) => this.pixels = el} children={this.state.children} />;
-    }
+  render() {
+    return (
+      <div
+        id="pixels"
+        ref={el => (this.pixels = el)}
+        children={this.state.children}
+      />
+    );
+  }
 }
 
 export default Pixels;
