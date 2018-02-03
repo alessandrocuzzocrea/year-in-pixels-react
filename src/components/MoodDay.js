3be635fc313a6ab1c3b3bfb3ac6d52fdf76070e6
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:20:35 +0100

    Run Prettier on MoodDay.js

--- src/components/MoodDay.js
+++ src/components/MoodDay.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const MoodDay = (props) => {
-        const { day, activeDay, dataMood } = props;
-        return <button
-            className={day === activeDay ? "active" : null}
-            data-mood={dataMood}
-            onClick={() => { props.setActiveDay(props.day) }}>
-        </button>
+const MoodDay = props => {
+  const { day, activeDay, dataMood } = props;
+  return (
+    <button
+      className={day === activeDay ? "active" : null}
+      data-mood={dataMood}
+      onClick={() => {
+        props.setActiveDay(props.day);
+      }}
+    />
+  );
 };
 
 MoodDay.propTypes = {
-    day: PropTypes.number.isRequired,
-    activeDay: PropTypes.number.isRequired,
-    dataMood: PropTypes.number.isRequired,
-    setActiveDay: PropTypes.func.isRequired,
+  day: PropTypes.number.isRequired,
+  activeDay: PropTypes.number.isRequired,
+  dataMood: PropTypes.number.isRequired,
+  setActiveDay: PropTypes.func.isRequired
 };
 
 export default MoodDay;
