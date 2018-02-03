90900c0968fdbc822a2de7435283d26d630d99ed
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:47:56 +0100

    Refactor MoodSelector component into a SFC

--- src/components/MoodSelector.js
+++ src/components/MoodSelector.js
@@ -3,42 +3,42 @@ import PropTypes from "prop-types";
 
 import "./MoodSelector.css";
 
-class MoodSelector extends React.Component {
-  render() {
-    const { activeMoodDayValue } = this.props;
-
-    // prettier-ignore
-    const moodButtons = [
-      "none", "tough", "difficult", "average", "great", "amazing"
-    ].map((value, i) => {
-      return (
-        <div
-          key={i}
-          onClick={() => {
-            this.props.changeDateMoodValue(i);
-          }}
-        >
-          <input
-            type="radio"
-            id={value}
-            name="dayMood"
-            value={i}
-            className={`mood-${i}`}
-            readOnly
-            checked={activeMoodDayValue === i}
-          />
-          <label htmlFor={value} />
-        </div>
-      );
-    });
-
+const getButtons = (activeMoodDayValue, changeDateMoodValue) => {
+  // prettier-ignore
+  return [
+    "none", "tough", "difficult", "average", "great", "amazing"
+  ].map((value, i) => {
     return (
-      <form id="submitMood" action="">
-        {moodButtons}
-      </form>
+      <div
+        key={i}
+        onClick={() => {
+          changeDateMoodValue(i);
+        }}
+      >
+        <input
+          type="radio"
+          id={value}
+          name="dayMood"
+          value={i}
+          className={`mood-${i}`}
+          readOnly
+          checked={activeMoodDayValue === i}
+        />
+        <label htmlFor={value} />
+      </div>
     );
-  }
-}
+  });
+};
+
+const MoodSelector = props => {
+  const { activeMoodDayValue, changeDateMoodValue } = props;
+
+  return (
+    <form id="submitMood" action="">
+      {getButtons(activeMoodDayValue, changeDateMoodValue)}
+    </form>
+  );
+};
 
 MoodSelector.propTypes = {
   changeDateMoodValue: PropTypes.func.isRequired,
