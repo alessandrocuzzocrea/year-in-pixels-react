85c005a040266e6e30345e9fbd10a414d68bd42e
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:19:52 +0100

    Run Prettier on AboutDialog.js

--- src/components/AboutDialog.js
+++ src/components/AboutDialog.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const AboutDialog = (props) => {
-
-    return (
-        <div id="aboutDialog" className="dialog" style={props.style}>
-            <a href="#" className="close" onClick={() => props.closeDialog()}>X</a>
-            <h3>year-in-pixels-react</h3>
-            <p>Remixed by <a href="https://twitter.com/alcuzzocrea" target="_blank" rel="noopener noreferrer">@alcuzzocrea</a></p>
-            <p>This is a complete React porting of <a href="https://year-in-pixels.glitch.me/" target="_blank" rel="noopener noreferrer">Year in Pixel</a></p>
-            <p><a href="https://github.com/alessandrocuzzocrea/year-in-pixels-react" target="_blank" rel="noopener noreferrer">Github</a></p>
-            <h4>Original App by:</h4>
-            <p>Alejandro AR (<a href="http://kinduff.com" target="_blank" rel="noopener noreferrer">@kinduff</a>)</p>
-        </div>
-    );
-}
+const AboutDialog = props => {
+  return (
+    <div id="aboutDialog" className="dialog" style={props.style}>
+      <a href="#" className="close" onClick={() => props.closeDialog()}>X</a>
+        <h3>year-in-pixels-react</h3>
+        <p>Remixed by <a href="https://twitter.com/alcuzzocrea" target="_blank" rel="noopener noreferrer">@alcuzzocrea</a></p>
+        <p>This is a complete React porting of <a href="https://year-in-pixels.glitch.me/" target="_blank" rel="noopener noreferrer">Year in Pixel</a></p>
+        <p><a href="https://github.com/alessandrocuzzocrea/year-in-pixels-react" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <h4>Original App by:</h4>
+        <p>Alejandro AR (<a href="http://kinduff.com" target="_blank" rel="noopener noreferrer">@kinduff</a>)</p>
+    </div>
+  );
+};
 
 export default AboutDialog;
