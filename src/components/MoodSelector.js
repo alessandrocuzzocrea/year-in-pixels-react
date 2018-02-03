6a5358e0a130d8a37cdeaa027d7a5a2b2bdd17fd
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:21:30 +0100

    Run Prettier on MoodSelector.js

--- src/components/MoodSelector.js
+++ src/components/MoodSelector.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './MoodSelector.css';
+import "./MoodSelector.css";
 
 class MoodSelector extends React.Component {
+  render() {
+    const { activeMoodDayValue } = this.props;
 
-    render() {
+    // prettier-ignore
+    const moodButtons = [
+      "none", "tough", "difficult", "average", "great", "amazing"
+    ].map((value, i) => {
+      return (
+        <div
+          key={i}
+          onClick={() => {
+            this.props.changeDateMoodValue(i);
+          }}
+        >
+          <input
+            type="radio"
+            id={value}
+            name="dayMood"
+            value={i}
+            className={`mood-${i}`}
+            readOnly
+            checked={activeMoodDayValue === i}
+          />
+          <label htmlFor={value} />
+        </div>
+      );
+    });
 
-        const {activeMoodDayValue} = this.props;
-
-        const moodButtons = 
-            ["none", "tough", "difficult", "average", "great", "amazing"]
-            .map((value, i) => {
-                return (
-                <div key={i} onClick={ () => {this.props.changeDateMoodValue(i)}}>
-                    <input type="radio" id={value} name="dayMood" value={i} className={`mood-${i}`} readOnly checked={activeMoodDayValue === i}/>
-                    <label htmlFor={value}></label>
-                </div>
-                );
-            });
-
-        return (
-            <form id="submitMood" action="">
-                { moodButtons }
-            </form>
-        );
-    }
+    return (
+      <form id="submitMood" action="">
+        {moodButtons}
+      </form>
+    );
+  }
 }
 
 MoodSelector.propTypes = {
-    changeDateMoodValue: PropTypes.func.isRequired,
-    activeMoodDayValue: PropTypes.number.isRequired,
+  changeDateMoodValue: PropTypes.func.isRequired,
+  activeMoodDayValue: PropTypes.number.isRequired
 };
 
 export default MoodSelector;
