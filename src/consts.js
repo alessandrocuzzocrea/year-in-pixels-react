405d6fd3a220dbc751428b7e12d78e871b7600bf
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2018-02-03 22:19:30 +0100

    Run Prettier on consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,11 +1,11 @@
 module.exports = {
-    moodOptions: ["none", "tough", "difficult", "average", "great", "amazing"],
-    QuotesRESTAPIUrl: "https://quotes.rest/qod",
-    clearDataMsg: "Careful, this will clear all the current data. Are you sure?",
-    importErrorMsg: "We're sorry.\nThe data is not valid. Please try again.",
-    dialogs: {
-        import: "import",
-        export: "export",
-        about: "about"
-    }
+  moodOptions: ["none", "tough", "difficult", "average", "great", "amazing"],
+  QuotesRESTAPIUrl: "https://quotes.rest/qod",
+  clearDataMsg: "Careful, this will clear all the current data. Are you sure?",
+  importErrorMsg: "We're sorry.\nThe data is not valid. Please try again.",
+  dialogs: {
+    import: "import",
+    export: "export",
+    about: "about"
+  }
 };
